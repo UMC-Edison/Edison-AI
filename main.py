6cadadb0060f0aee28fefc91d953d64555bc40db
Claire <113418806+Sunho12@users.noreply.github.com>
2025-11-06 11:03:11 +0900

    ❗️HOTFIX: 유사도 기준 변경

--- main.py
+++ main.py
@@ -116,8 +116,8 @@ def calculate_similarity(req: SimilarityRequest):
         sim = float(cosine_similarity([keyword_vec], [memo_vec])[0][0])
         print(f"키워드: {req.keyword}, 메모: {memo.content[:30]}..., 유사도: {sim}")
 
-        if sim >= 0.0:
-            scored.append(SimilarityResultItem(localIdx=memo.localIdx, similarity=sim))
+        # if sim >= 0.0:
+        scored.append(SimilarityResultItem(localIdx=memo.localIdx, similarity=sim))
 
     sorted_results = sorted(scored, key=lambda x: x.similarity, reverse=True)
 
